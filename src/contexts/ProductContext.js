ce1688d4ba6a99c87a160ea58d59e02ebc34d668
thesoundfromthesky <nagara@naver.com>
2019-12-09 09:26:04 -0600

    Step 2 - creating ProductContext

---
+++ src/contexts/ProductContext.js
@@ -0,0 +1,13 @@
+import { createContext } from "react";
+
+/*
+**STEP 2 - Creating ProductContext**
+
+- In `src`, create a new folder named `contexts`, this folder is going to be used to hold all of `context objects` we create.
+
+- Inside that folder create a new file named `ProductContext.js`
+
+- In this file, import the `createContext` function from the react library and create our `ProductContext`.
+*/
+
+export const ProductContext = createContext();
